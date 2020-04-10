3df7f252204a7959467aec8d2038f7694d0fb2bb
Avi Huly <avihuly@gmail.com>
2020-04-10 23:00:41 +0300

    vendoring

--- yaml/deploy.sh
+++ yaml/deploy.sh
@@ -1,9 +1,11 @@
 oc delete mutatingwebhookconfiguration.admissionregistration.k8s.io/inject-logstach-api-logger-sidecar
 oc delete secret ssl
+oc delete deployment mutate-server
 
 oc delete service petclinic -n myproject
 oc delete deployment petclinic -n myproject
 
 sleep 3
 oc create secret generic ssl --from-file mutateme-server.pem=./ssl/mutateme-server.pem --from-file mutateme-server.key=./ssl/mutateme-server.key
+oc apply -f yaml/mutate-server.yaml -n controllers
 oc apply -f yaml/MutatingWebhookConfiguration.yaml
